fca58d846640b834ceb9444690c3b3d55656f4bc
Daniel Avery <daniel@hub.uconnect.utah.edu>
2017-11-27 16:15:40 -0700

    Add second motor control

--- STMChassis/Inc/motor.h
+++ STMChassis/Inc/motor.h
@@ -8,41 +8,19 @@
  *  Global Variable and Type Declarations
  *  -------------------------------------------------------------------------------------------------------------
  */
-extern volatile int8_t y_duty_cycle;    // Output PWM duty cycle
-extern volatile int16_t y_target_rpm;    // Desired speed target
-extern volatile int16_t y_motor_speed;   // Measured motor speed
-extern volatile int16_t y_error;         // Speed error signal
-extern volatile int16_t y_error_integral;// Integrated error signal
-extern volatile int16_t y_pos;
 
-extern volatile int8_t z_duty_cycle;    // Output PWM duty cycle
-extern volatile int16_t z_target_rpm;    // Desired speed target
-extern volatile int16_t z_motor_speed;   // Measured motor speed
-extern volatile int16_t z_error;         // Speed error signal
-extern volatile int16_t z_error_integral;// Integrated error signal
-extern volatile int16_t z_pos;
-
-extern volatile uint8_t Kp;            // Proportional gain
-extern volatile uint8_t Ki;            // Integral gain
-
+#define LEFT_MOTOR 0
+#define RIGHT_MOTOR 1
 
 /* -------------------------------------------------------------------------------------------------------------
  *  Motor Control and Initialization Functions
  * -------------------------------------------------------------------------------------------------------------
  */
 
-// Sets up the entire motor drive system
-void Motor_Init(void);
-
-// Set the duty cycle of the PWM, accepts (0,100)
-void PWM_set_duty_cycle(uint32_t duty);
-
-/* -------------------------------------------------------------------------------------------------------------
- *  Internal-Use Initialization Functions
- * -------------------------------------------------------------------------------------------------------------
- */
-
-// Sets up the PWM and direction signals to drive the H-Bridge
+// Sets up the PWM hardware
 void PWM_Init(void);
+
+// Set the duty cycle of a particular motor PWM, accepts (0,100)
+void PWM_set_duty_cycle(uint8_t motor_id, uint8_t duty);
 
 #endif /* MOTOR_H_ */
